23580912e94d65af95dd6a4fef72d9dbc1626e4e
fleetimee <novian.andika@students.amikom.ac.id>
2022-12-01 14:46:23 +0700

    feat: ready for merging

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -8,7 +8,7 @@ export class CloudinaryService {
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream((error, result) => {
+      const upload = v2.uploader.upload_stream({}, (error, result) => {
         if (error) return reject(error);
         resolve(result);
       });
